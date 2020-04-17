88bfaf9e0e4914502c549aab2316697f141fba06
Eloy <eloydgutierrez@gmail.com>
2020-04-16 20:36:23 -0400

    added code to meet MVP

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,11 +11,12 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="App">
+    <div data-testid="app-div" className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            data-testid="required-fName"
             name="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +29,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            data-testid="required-lName"
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
@@ -41,21 +43,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input data-testid="required-email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea data-testid="text-area" name="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submit-button" type="submit" />
       </form>
     </div>
   );
